95fa6f254be6d7145cf222be5ec231387847e00c
satyanarayana <satti9390938369@gmail.com>
2026-03-05 16:48:30 +0530

    update postgres connection for render

--- src/db.js
+++ src/db.js
@@ -2,11 +2,7 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: "shoppingcart_db_user",
-  host: "dpg-d6kjbmd6ubrc73ehf92g-a.oregon-postgres.render.com",
-  database: "shoppingcart_db",
-  password: "foZ32pLQWCDvZmTFZ7XWJFXFPpSfWDTs",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
